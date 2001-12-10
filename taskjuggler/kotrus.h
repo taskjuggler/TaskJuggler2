7daacc4a31b3207bd559f8e6c107d26890f5737b
freitag <freitag@e1914e07-63f8-0310-9059-d6d858d7cdca>
2001-12-10 13:01:09 +0000

    new kotrus class
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@20 e1914e07-63f8-0310-9059-d6d858d7cdca

---
+++ taskjuggler/kotrus.h
@@ -0,0 +1,67 @@
+/*
+ * Kotrus.h - TaskJuggler
+ *
+ * Copyright (c) 2001 by Chris Schlaeger <cs@suse.de>
+ *                       Klaas Freitag <freitag@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * $Id$
+ */
+
+#include "ResourceList.h"
+
+class QSqlDatabase;
+
+class Kotrus
+{
+public:
+   Kotrus( );
+
+   ~Kotrus( );
+
+   /**
+    * @returns the database person id on a given name
+    */
+   int personID( const QString& );
+
+   /**
+    * @returns the database-'Kostenträger'-id on a given name
+    */
+   int ktID( const QString& );
+
+   /**
+    * @returns a list of bookings for the person identified by the kotrusID
+    * for all projects except the one given in skipProjectID. If skipProjectID
+    * is 0, all projects are in the return list.
+    * The member task remains emtpy in the bookings.
+    * @param kotrusID is the email address of the user for whom the projects should be loaded
+    * @param skipProjectID points to a project which should be excluded. Pass zero for getting
+    *        all projects.
+    * @param user is the user ID of the person locking the bookings.
+    *
+    */
+   BookingList loadBookings( const QString& kotrusID, const QString& skipProjectID, int user=0 );
+
+   int         saveBookings( const QString& kotrusID , const QString& projectID,
+			     const BookingList&, int lockedFor  );
+
+   int 	       getKotrusAccountId( const QString& acc );
+   void        unlockBookings( const QString& kotrusID );
+   int         lockBookings( int personID, int lockID );
+
+private:
+   /**
+    * locks the bookings for a person with the id of the locker. Pass zero for lockID to
+    * unlock the entries.
+    */
+   
+   
+   void 		connect();
+   QString 		Param( QString key ) const;
+   
+   QSqlDatabase 	*kotrusDB;
+   
+};
