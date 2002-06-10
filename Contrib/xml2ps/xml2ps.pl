89703c4f76d1f39c2e63b73f295e16114518832c
ray <ray@e1914e07-63f8-0310-9059-d6d858d7cdca>
2002-06-10 16:23:05 +0000

    ...
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@171 e1914e07-63f8-0310-9059-d6d858d7cdca

--- Contrib/xml2ps/xml2ps.pl
+++ Contrib/xml2ps/xml2ps.pl
@@ -27,22 +27,14 @@ package tjTask;
     use Class::MethodMaker
         new_hash_init   => 'new',
         get_set         => [ qw(Index ProjectID complete Priority Type Id Name ParentTask
-                                minStart
-                                maxStart
-                                minEnd
-                                maxEnd
-                                actualStart
-                                actualEnd
-                                planStart
-                                planEnd
-                                h_minStart
-                                h_maxStart
-                                h_minEnd
-                                h_maxEnd
-                                h_actualStart
-                                h_actualEnd
-                                h_planStart
-                                h_planEnd
+                                minStart maxStart
+                                minEnd maxEnd
+                                actualStart actualEnd
+                                planStart planEnd
+                                h_minStart h_minEnd
+                                h_maxStart h_maxEnd
+                                h_actualStart h_actualEnd
+                                h_planStart h_planEnd
                                 x1 y1 x2 y2 ) ],
         struct          => [ qw(Followers Depends Previous Allocations bookedResources) ];
 
