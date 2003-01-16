1276d868c1e68c1bf7d02151292a1240b1a567a0
cs <cs@e1914e07-63f8-0310-9059-d6d858d7cdca>
2003-01-16 08:32:53 +0000

    - Added missing file.
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@401 e1914e07-63f8-0310-9059-d6d858d7cdca

---
+++ taskjuggler/ReportElement.h
@@ -0,0 +1,23 @@
+/*
+ * ReportElement.h - TaskJuggler
+ *
+ * Copyright (c) 2001, 2002, 2003 by Chris Schlaeger <cs@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * $Id$
+ */
+#ifndef _ReportElement_h_
+#define _ReportElement_h_
+
+class ReportElement
+{
+public:
+	ReportElement() { }
+	~ReportElement() { }
+} ;
+
+#endif
+
