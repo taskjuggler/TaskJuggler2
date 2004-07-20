458d20d18d5c25785ec001969b05a5368ba978ec
ltinkl <ltinkl@e1914e07-63f8-0310-9059-d6d858d7cdca>
2004-07-20 09:14:22 +0000

    date tag
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@745 e1914e07-63f8-0310-9059-d6d858d7cdca

--- ktjview2/main.cpp
+++ ktjview2/main.cpp
@@ -28,7 +28,7 @@
 static const char description[] =
     I18N_NOOP("A KDE frontend for TaskJuggler.");
 
-static const char version[] = "0.1";
+static const char version[] = "0.1 $Date$";
 
 static KCmdLineOptions options[] =
 {
