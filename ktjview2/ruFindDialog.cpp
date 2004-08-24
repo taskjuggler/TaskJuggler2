5bdbe30987faf458d3409e2937bbd67a1be5e221
ltinkl <ltinkl@e1914e07-63f8-0310-9059-d6d858d7cdca>
2004-08-24 23:02:26 +0000

    copyright + docu
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@796 e1914e07-63f8-0310-9059-d6d858d7cdca

--- ktjview2/ruFindDialog.cpp
+++ ktjview2/ruFindDialog.cpp
@@ -1,5 +1,27 @@
+// -*- Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; -*-
+/***************************************************************************
+ *   Copyright (C) 2004 by Lukas Tinkl                                     *
+ *   lukas.tinkl@suse.cz                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+// local includes
 #include "ruFindDialog.h"
 
+// Qt includes
 #include <qbuttongroup.h>
 #include <qlabel.h>
 #include <qcheckbox.h>
@@ -7,6 +29,7 @@
 #include <qlayout.h>
 #include <qregexp.h>
 
+// KDE includes
 #include <kdialog.h>
 #include <klocale.h>
 #include <kdebug.h>
--- ktjview2/ruFindDialog.h
+++ ktjview2/ruFindDialog.h
@@ -1,3 +1,24 @@
+// -*- Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; -*-
+/***************************************************************************
+ *   Copyright (C) 2004 by Lukas Tinkl                                     *
+ *   lukas.tinkl@suse.cz                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifndef _RU_FIND_DIALOG_H_
 #define _RU_FIND_DIALOG_H_
 
@@ -10,10 +31,20 @@ class QGridLayout;
 
 #include <kdialogbase.h>
 
+/**
+ * This class provides a find dialog for the Resource Usage view (ResUsageView)
+ *
+ * @author Lukas Tinkl <lukas.tinkl@suse.cz>
+ * @short Find dialog for the Resource Usage view
+ */
 class ruFindDlg : public KDialogBase
 {
     Q_OBJECT
 public:
+    /**
+     * CTOR
+     * @param data the stringlist to work on
+     */
     ruFindDlg( QStringList data, QWidget* parent = 0, const char* name = 0 );
     ~ruFindDlg();
 
@@ -58,7 +89,7 @@ private:
     QStringList m_data;
     /// the result list of column numbers
     QValueList<int> m_result;
-
+    /// distinguish find/find next
     bool m_firstRun;
 };
 
