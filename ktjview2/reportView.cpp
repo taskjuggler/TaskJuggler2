cc907681fa6668b8df8aba19f945356fdc921dc6
ltinkl <ltinkl@e1914e07-63f8-0310-9059-d6d858d7cdca>
2004-10-03 19:08:46 +0000

    headers will use the model, hopefully
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@867 e1914e07-63f8-0310-9059-d6d858d7cdca

--- ktjview2/reportView.cpp
+++ ktjview2/reportView.cpp
@@ -8,6 +8,8 @@ ReportView::ReportView( QWidget *parent, const char *name )
     : QicsTable( 0, parent, name )
 {
     setReadOnly( true );
+    setRowHeaderUsesModel( true );
+    setColumnHeaderUsesModel( true );
 }
 
 #include "reportView.moc"
