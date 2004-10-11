52986a18f6f16cda62ae9d159efa7df0f53b8d23
ltinkl <ltinkl@e1914e07-63f8-0310-9059-d6d858d7cdca>
2004-10-11 15:20:09 +0000

    add copyrights + some more docu
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@892 e1914e07-63f8-0310-9059-d6d858d7cdca

--- ktjview2/ktjReportDialog.h
+++ ktjview2/ktjReportDialog.h
@@ -1,3 +1,24 @@
+// -*- Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; -*-
+/***************************************************************************
+ *   Copyright (C) 2004 by Lukas Tinkl                                     *
+ *   lukas.tinkl@suse.cz                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifndef _KTJREPORTDIALOG_H_
 #define _KTJREPORTDIALOG_H_
 
@@ -9,11 +30,26 @@ class QDateTime;
 
 #include <kdialogbase.h>
 
+/**
+ * @short Report options dialog
+ *
+ * Dialog for setting options and generating a report
+ * @author Lukas Tinkl <lukas.tinkl@suse.cz>
+ */
 class KtjReportDialog: public KDialogBase
 {
     Q_OBJECT
 public:
+    /**
+     * Constructor
+     * @param start start of the timeframe
+     * @param end end of the timeframe
+     */
     KtjReportDialog( const QDateTime & start, const QDateTime & end, QWidget * parent = 0, const char * name = 0 );
+
+    /**
+     * Destructor
+     */
     ~KtjReportDialog();
 
     /**
@@ -36,9 +72,13 @@ public:
      */
     void setEndDate( const QDateTime & date );
 
+    /**
+     * @return the current scale
+     */
     KtjReport::Scale scale() const;
 
 private:
+    /// base widget
     ReportSelectionWidget * m_base;
 };
 
