81169ea75ce4aa4b3666cea50f2f6202ec47de7d
cs <cs@e1914e07-63f8-0310-9059-d6d858d7cdca>
2005-01-21 14:01:53 +0000

    - added missing file.
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@967 e1914e07-63f8-0310-9059-d6d858d7cdca

---
+++ taskjuggler/ResourceScenario.h
@@ -0,0 +1,42 @@
+/*
+ * ResourceScenario.h - TaskJuggler
+ *
+ * Copyright (c) 2001, 2002, 2003, 2004, 2005 by Chris Schlaeger <cs@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * $Id$
+ */
+
+#ifndef _ResourceScenario_h_
+#define _ResourceScenario_h_
+
+#include "TaskList.h"
+#include "Task.h"
+
+class ResourceScenario
+{
+    friend class Resource;
+
+public:
+    ResourceScenario()
+    {
+        firstSlot = lastSlot = -1;
+    }
+    ~ResourceScenario() { }
+
+    void addTask(const Task* t)
+    {
+        if (allocatedTasks.findRef(t) < 0)
+            allocatedTasks.append(t);
+    }
+private:
+    TaskList allocatedTasks;
+    int firstSlot;
+    int lastSlot;
+} ;
+
+#endif
+
