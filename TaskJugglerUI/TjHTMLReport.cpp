ce717c352eb9c2d918dc5d8c418d4bc5aabf7daa
cs <cs@e1914e07-63f8-0310-9059-d6d858d7cdca>
2005-10-30 04:20:02 +0000

    - Add new class as well.
    git-svn-id: https://www.taskjuggler.org/svn/taskjuggler/trunk@1188 e1914e07-63f8-0310-9059-d6d858d7cdca

---
+++ TaskJugglerUI/TjHTMLReport.cpp
@@ -0,0 +1,90 @@
+/*
+ * The TaskJuggler Project Management Software
+ *
+ * Copyright (c) 2001, 2002, 2003, 2004, 2005 by Chris Schlaeger <cs@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * $Id: TjReport.h 1181 2005-10-24 08:29:54Z cs $
+ */
+
+#include "TjHTMLReport.h"
+
+#include <qlayout.h>
+
+#include <klocale.h>
+#include <khtml_part.h>
+#include <khtmlview.h>
+#include <krun.h>
+
+#include <HTMLReport.h>
+
+TjHTMLReport::TjHTMLReport(QWidget* p, Report* rDef,
+                                 const QString& n) :
+    TjReportBase(p, rDef, n)
+{
+    QVBoxLayout* hl = new QVBoxLayout(this, 0);
+    hl->setAutoAdd(true);
+    browser = new KHTMLPart(this);
+
+    /* It is very unlikely that we need these advanced KHTML features, so
+     * let's disable them for security reasons. */
+    browser->setJScriptEnabled(false);
+    browser->setJavaEnabled(false);
+    browser->setMetaRefreshEnabled(false);
+    browser->setPluginsEnabled(false);
+    browser->setOnlyLocalReferences(true);
+
+    connect(browser->browserExtension(),
+            SIGNAL(openURLRequest(const KURL &, const KParts::URLArgs&)),
+            this, SLOT(openURLRequest(const KURL &, const KParts::URLArgs&)));
+    connect(browser, SIGNAL(onURL(const QString&)),
+            this, SLOT(showURLinStatusBar(const QString&)));
+}
+
+bool
+TjHTMLReport::generateReport()
+{
+    HTMLReport* htmlReport = dynamic_cast<HTMLReport*>(reportDef);
+    if (!htmlReport->generate())
+        return false;
+    KURL reportURL = KURL::fromPathOrURL(reportDef->getFullFileName());
+    browser->openURL(reportURL);
+
+    return true;
+}
+
+void
+TjHTMLReport::print()
+{
+    browser->view()->print();
+}
+
+void
+TjHTMLReport::show()
+{
+    QWidget::show();
+}
+
+void
+TjHTMLReport::hide()
+{
+    QWidget::hide();
+}
+
+void
+TjHTMLReport::showURLinStatusBar(const QString& url)
+{
+    emit signalChangeStatusBar(url);
+}
+
+void
+TjHTMLReport::openURLRequest(const KURL &url, const KParts::URLArgs&)
+{
+    KRun::runURL(url, "text/html");
+}
+
+#include "TjHTMLReport.moc"
+
---
+++ TaskJugglerUI/TjHTMLReport.h
@@ -0,0 +1,53 @@
+/*
+ * The TaskJuggler Project Management Software
+ *
+ * Copyright (c) 2001, 2002, 2003, 2004, 2005 by Chris Schlaeger <cs@suse.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * $Id: TjReport.h 1181 2005-10-24 08:29:54Z cs $
+ */
+
+#ifndef _TjHTMLReport_h_
+#define _TjHTMLReport_h_
+
+#include "TjReportBase.h"
+
+#include <kparts/browserrun.h>
+
+class KURL;
+class KHTMLPart;
+class Project;
+class Report;
+
+class TjHTMLReport : public TjReportBase
+{
+    Q_OBJECT
+
+public:
+    TjHTMLReport(QWidget* p, Report* rDef,
+                 const QString& n = QString::null);
+    virtual ~TjHTMLReport() { }
+
+    virtual bool generateReport();
+
+    virtual void print();
+
+public slots:
+    virtual void show();
+    virtual void hide();
+
+    void showURLinStatusBar(const QString& url);
+    void openURLRequest(const KURL &url, const KParts::URLArgs&);
+
+protected:
+    TjHTMLReport() { }
+
+private:
+    KHTMLPart* browser;
+} ;
+
+#endif
+
